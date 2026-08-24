4d3dba08db7920c1b377de630e85af669b7b0e8e
peanutboi710 <figaro0928@gmail.com>
2026-08-24 17:39:14 +0900

    feat: 최초생성

---
+++ src/main/java/org/example/matcheat/MatchEatApplication.java
@@ -0,0 +1,13 @@
+package org.example.matcheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchEatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/matcheat/MatchEatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.matcheat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
