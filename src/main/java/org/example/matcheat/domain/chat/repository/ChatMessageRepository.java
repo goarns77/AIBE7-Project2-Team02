01d5dd0b45c90761ed5852a413b0b689c5aa85de
peanutboi710 <figaro0928@gmail.com>
2026-08-26 12:45:26 +0900

    feat: WebSocket 초기설정

--- src/main/java/org/example/matcheat/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/org/example/matcheat/domain/chat/repository/ChatMessageRepository.java
@@ -1,4 +1,10 @@
 package org.example.matcheat.domain.chat.repository;
 
-public class ChatMessageRepository {
+import org.example.matcheat.domain.chat.entity.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+	// 채팅방 이전 메시지 내역 조회용
+	List<ChatMessage> findByChatRoomIdOrderByCreatedAtAsc(Long chatRoomId);
 }
